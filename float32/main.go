d9c4880550b355bb4d193de3211bc136eca947b2
nyanmy <nyanmyth100@gmail.com>
2023-01-30 18:54:58 +0900

    1/30

---
+++ float32/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func Float32Internal(n float32) string {
+
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ieee754 <floating-point-number>")
+	}
+
+	f, err := strconv.ParseFloat(os.Args[1], 32)
+
+	if err != nil {
+		fmt.Println("Command line parameter must be floating point number")
+		return
+	}
+
+	// Float32Internal文字列を返す
+	fmt.Print(Float32Internal(float32(f)))
+}
