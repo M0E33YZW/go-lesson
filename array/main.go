3f1539af638c001540d7153e773081ce98adc418
nyanmy <nyanmyth100@gmail.com>
2023-04-03 00:55:59 +0900

    makeNewArray 修正

--- array/main.go
+++ array/main.go
@@ -6,10 +6,8 @@ func makeNewArray(in []int) {
 	out := []int{}
 	out = append ([] int{in[0]})
 
-	for i := 2; i < len(in); i++ {
-		var outLen = len(out)
-		var tail int = out[outLen -1]
-		fmt.Print(tail)
+	for i := 1; i < len(in); i++ {
+		tail := out[len(out) - 1]
 		out = append(out, tail + in[i])
 	}
 	fmt.Print(out)
